90ab82306e522c4aabff599a7abd9f3bc2870aa1
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2025-09-15 04:13:22 +0530

    Completed Set-up for user signup and signin process with JWT

---
+++ Backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+const db = require('./models/index');
+const {User, Role} = require('./models/index');
+
+//const UserService = require('./services/user-service');
+
+const app = express();
+
+const prepareAndStartServer = () => {
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
+    app.listen(PORT, async () => {
+        console.log("Server Started on PORT:",PORT);
+         if(process.env.DB_SYNC) {
+             db.sequelize.sync({alter: true});
+         }
+    });
+}
+
+prepareAndStartServer();
---
+++ Backend/src/services/user-service.js
@@ -0,0 +1,94 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const UserRepository = require('../repository/user-repository');
+const { JWT_KEY } = require('../config/serverConfig');
+const AppErrors = require('../utils/error-handler');
+
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async create(data) {
+        try {
+            const user = await this.userRepository.create(data);
+            return user;
+        } catch (error) {
+            if(error.name == 'SequelizeValidationError') {
+                throw error;
+            }
+            console.log("Something went wrong in service layer");
+            throw error;
+        }
+    }
+
+    async signIn(email, plainPassword) {
+        try {
+            // step 1: fetch the user using email
+            const user = await this.userRepository.getByEmail(email);
+            // step 2: compare incoming plain password with stored encrypted password
+            const passwordMatch = this.checkPassword(plainPassword, user.password);
+            if(!passwordMatch) {
+                console.log("Password doesn't match ");
+                throw {error: 'Incorrect password'};
+            }
+            // step 3: if passwords match then create a token and send it to the user
+            const newJWT = this.createToken({email: user.email, id: user.id});
+            return newJWT;
+        } catch (error) {
+            if(error.name == 'AttributeNotFound') {
+                throw error;
+            }
+            console.log("Something went wron in service layer");
+            throw error;
+        }
+    }
+
+    async isAuthenticated(token) {
+        try {
+            const response = this.verifyToken(token);
+            if(!response) {
+                throw {error: 'Invalid User'}
+            }
+            const user = this.userRepository.getById(response.id);
+            if(!user){
+                throw {error: 'No user with the corresponding toke exists'};
+            }
+            return user.id;
+        } catch (error) {
+            console.log("Something went wron in service layer");
+            throw error;
+        } 
+    }
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1h'});
+            return result;
+        } catch (error) {
+            console.log("Something went wron in service layer");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wron in service layer");
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log("Something went wron in service layer");
+            throw error;
+        }
+    }
+}
+
+module.exports = UserService;
